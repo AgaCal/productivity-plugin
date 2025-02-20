5802e0627e5cfff50a476cc2efe89070abb532b7
aga <calkowska.aga@gmail.com>
2025-02-20 18:12:38 -0500

    Created productivity reminders

--- main.ts
+++ main.ts
@@ -1,39 +1,39 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile, TFolder, Vault } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface allSettings {
+	frequency: number, // frequency of reminders in minutes
+	duration: number // duration of notice in seconds
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: allSettings = {
+	frequency: 30, 
+	duration: 5 
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TestPlugin extends Plugin {
+	settings: allSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Get random task', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new RandomizerModal(this.app).open();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Status Bar Text');`	`
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-randomizer',
+			name: 'Open Randomizer)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new RandomizerModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -56,7 +56,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new RandomizerModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,20 +66,39 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new ReminderSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.startReminders();
 	}
 
 	onunload() {
 
+	}
+
+	startReminders() {
+		let folder = this.app.vault.getFolderByPath("assets");
+
+		if (folder == null) {
+			new Notice("assets folder doesn't exist! make one directly inside the vault folder, and put in some images, or else you won't get productivity reminders", 10 * 1000);
+		} else {
+			let assets_folder : TFile[] = [];
+		
+			folder.children.forEach((file)=> {
+				if (file instanceof TFile && (file.extension == "png" || file.extension == "jpeg")) {
+					assets_folder.push(file);
+				}
+			});
+
+			this.registerInterval(window.setInterval(() => {
+				new Notice('are you being productive?', (this.settings.duration * 1000))
+				.noticeEl.createEl("img", { attr: { 
+					src: this.app.vault.getResourcePath(assets_folder[Math.floor(Math.random()*assets_folder.length)]),
+					width: 150, 
+					height: 150,
+					objectfit: "cover"}
+				});
+			}, this.settings.frequency * 60 * 1000));
+		}
 	}
 
 	async loadSettings() {
@@ -91,14 +110,14 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class RandomizerModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('in progress!');
 	}
 
 	onClose() {
@@ -107,10 +126,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ReminderSettingTab extends PluginSettingTab {
+	plugin: TestPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TestPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +140,32 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Reminder Frequency')
+			.addSlider((number) => 
+				number
+					.setValue(this.plugin.settings.frequency)
+					.onChange(async (value) => {
+						this.plugin.settings.frequency = 1 + 59 * value/100;
+						await this.plugin.saveSettings();
+					})
+		);
+
+		new Setting(containerEl)
+			.setName('Reminder Duration')
+			.addSlider((number) => 
+				number
+					.setValue(this.plugin.settings.duration)
+					.onChange(async (value) => {
+						this.plugin.settings.duration = 1 + 59 * value/100;
+						await this.plugin.saveSettings();
+					})
+		);
 	}
 }
+
+
+/*
+
+
+
+*/
